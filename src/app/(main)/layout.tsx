b7850df345ab2894d6df0bbf2617552c97fdadc9
revyid <revy8k@gmail.com>
2026-07-08 19:40:25 +0700

    fix: hapus dynamic import MainShell - fix double mount PortfolioProvider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const MainShell = dynamic(() => import('./shell').then(m => ({ default: m.MainShell })), { ssr: false });
+import { MainShell } from './shell';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return <MainShell>{children}</MainShell>;
